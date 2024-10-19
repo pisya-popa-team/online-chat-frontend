3549f6c1b609452090bc58887e992f2d0ea50f89
Son1kXDev <son1kx@vk.com>
2024-10-19 21:46:46 +0300

    feat: верстка регистрации

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegComponent } from './pages/reg/reg.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'online-chat-frontend';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { RegComponent } from './pages/reg/reg.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'reg', component: RegComponent }];
