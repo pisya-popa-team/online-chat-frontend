d30fd6eb3e06580e69b8e76391fd7000e58188d8
Son1kXDev <son1kx@vk.com>
2024-10-25 03:42:37 +0300

    feat: верстка авторизации

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegComponent } from './pages/reg/reg.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
-export const routes: Routes = [{ path: 'reg', component: RegComponent }];
+export const routes: Routes = [
+  { path: 'reg', component: RegComponent },
+  { path: 'auth', component: AuthComponent },
+];
---
+++ src/app/pages/auth/auth.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent implements OnInit {
+  authForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.authForm.valid) {
+      console.log(this.authForm.value);
+    } else {
+      console.error('Форма недействительна');
+    }
+  }
+}
