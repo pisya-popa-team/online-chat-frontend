90087ec71ddae3f34d485e351b1c09b42e87bda8
Son1kXDev <son1kx@vk.com>
2024-10-25 03:16:03 +0300

    feat: добавил функционал к форме регистрации

--- src/app/pages/reg/reg.component.ts
+++ src/app/pages/reg/reg.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reg',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reg.component.html',
   styleUrl: './reg.component.css',
 })
-export class RegComponent {}
+export class RegComponent implements OnInit {
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      nickname: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    } else {
+      console.error('Форма недействительна');
+    }
+  }
+}
