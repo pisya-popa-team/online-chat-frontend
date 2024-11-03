7a26cdafcda2bb103e7dffe89a7c36f1a71f4a3a
Son1kXDev <son1kx@vk.com>
2024-11-03 16:38:52 +0300

    feat: перенос верстки авторизации и регистрации в tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      dark: "#333333",
+      "dark-90": "#333333E5",
+      "dark-80": "#333333CC",
+      "dark-60": "#33333399",
+      link: "#3BC9DB",
+      text: "#fdfdfd",
+      "text-muted": "#FFFFFF80",
+      brand: "#0c8498",
+      "brand-60": "#0C849899",
+      "brand-40": "#0C849866",
+      "brand-10": "#0C849819",
+      danger: "#f85151",
+      glass: "#C5F6FA33",
+      white: "#fdfdfd",
+      "white-60": "#FDFDFD99",
+      "white-50": "#FDFDFD7F",
+    },
+    extend: {
+      backgroundImage: {
+        auth: "url(/images/auth-bg.png)",
+        main: "url(/images/main-bg.png)",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        15: "15px",
+      },
+      maxWidth: {
+        form: "480px",
+      },
+      borderRadius: {
+        20: "20px",
+      },
+    },
   },
   plugins: [],
 };
