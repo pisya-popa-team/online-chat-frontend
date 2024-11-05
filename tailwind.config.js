746763b6795bc36d3be10d0d7633d195b37b1de4
Son1kXDev <son1kx@vk.com>
2024-11-05 21:32:17 +0300

    fix: цвета различных элементов

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,13 @@ module.exports = {
   theme: {
     colors: {
       dark: "#333333",
-      "dark-90": "#333333E5",
-      "dark-80": "#333333CC",
-      "dark-60": "#33333399",
       link: "#3BC9DB",
       text: "#fdfdfd",
-      "text-muted": "#FFFFFF80",
+      muted: "#FFFFFF80",
       brand: "#0c8498",
-      "brand-60": "#0C849899",
-      "brand-40": "#0C849866",
-      "brand-10": "#0C849819",
       danger: "#f85151",
       glass: "#C5F6FA33",
       white: "#fdfdfd",
-      "white-60": "#FDFDFD99",
-      "white-50": "#FDFDFD7F",
     },
     extend: {
       backgroundImage: {
