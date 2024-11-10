fcdabd4946dbc73216f01277dc364f12b618270c
Son1kXDev <son1kx@vk.com>
2024-11-10 17:57:10 +0300

    feat: добавил api url

--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class RegistrationService {
-  readonly api = '';
+  readonly api = 'https://api-tt-chat.danyatochka.ru/';
 
   httpClient = inject(HttpClient);
 }
