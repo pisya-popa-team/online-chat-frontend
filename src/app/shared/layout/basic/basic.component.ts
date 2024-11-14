9fcacb566c37c0c2a813f484f365afe89602862f
Son1kXDev <son1kx@vk.com>
2024-11-14 18:27:21 +0300

    feat: перенаправление на главную страницу при нажатии на лого

--- src/app/shared/layout/basic/basic.component.ts
+++ src/app/shared/layout/basic/basic.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { NgIf } from '@angular/common';
-import {RouterOutlet} from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-basic',
   standalone: true,
-  imports: [NgIf, RouterOutlet],
+  imports: [NgIf, RouterOutlet, RouterLink],
   templateUrl: './basic.component.html',
   styleUrl: './basic.component.css',
 })
